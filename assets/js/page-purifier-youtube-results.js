bc6cd969a61ad866bf71648a579dcf0b02522a4d
John Torakis <john.torakis@gmail.com>
2022-09-11 14:10:35 +0300

    further compacted the code

--- assets/js/page-purifier-youtube-results.js
+++ assets/js/page-purifier-youtube-results.js
@@ -1,97 +1,3 @@
-targetNode = null
-triggers = null
-
-browser.storage.sync.get(['triggers'])
-    .then((result) => {
-        triggers = result.triggers;
-		console.log("Trigger words loaded: " + Object.keys(triggers).length)
-    });
-
-// Taken from:
-// https://stackoverflow.com/a/39905590
-/*
-String.prototype.fuzzy = function(term, ratio=0.7) {
-    var string = this.toLowerCase();
-    var compare = term.toLowerCase();
-    var matches = 0;
-    if (string.indexOf(compare) > -1) return true; // covers basic partial matches
-    for (var i = 0; i < compare.length; i++) {
-        string.indexOf(compare[i]) > -1 ? matches += 1 : matches -=1;
-    }
-    return (matches/this.length >= ratio || term == "")
-};
-*/
-
-// Taken from:
-// https://stackoverflow.com/a/9310752
-function escapeRegExp(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-}
-
-function removeElement(elem) {
-	/*
-		Maybe add feature to replace element with custom HTML
-		in the future
-	*/
-	elem.remove()
-	// elem.innerText = "This content has been hidden by plugin"
-}
-
-function containsTrigger(element){
-	// console.log("-> Checking:")
-	console.log(element.innerText)
-
-	for (let trigger_word in triggers){
-		regex_flags = "m"	// multiline is the default
-		key = trigger_word
-
-		checked_text = element.innerText
-		console.log(`[+] Checking for '${trigger_word}'...`)
-		console.log(triggers[trigger_word])
-
-		// console.log("[1] Checking case_sensitive")
-		// If case_sensitive is NOT set - to lower case
-		if (!triggers[key]["case_sensitive"]) {
-			regex_flags += "i"
-		}
-
-		// console.log("[2] Checking whole_word")
-		// If whole_word is set - create a regex with greedy whitespaces
-		if (triggers[key]["whole_word"]) {
-			trigger_word = `\\s+${trigger_word}\\s+`
-			console.log(`[2][1] Whole word ${trigger_word}`)
-		}
-
-		// If regex is not set - escape all regex characters
-		// console.log("[3] Checking regex")
-		if (!triggers[key]["regex"]) {
-			console.log("[3][1] Escape regex chars")
-			trigger_word = escapeRegExp(trigger_word)
-		}
-
-		console.log(`[+] Creating regex for '${trigger_word}'`)
-		trigger_word = new RegExp(trigger_word, regex_flags)
-
-		// console.log("[!] Checking existence: ")
-		console.log(checked_text.match(trigger_word))
-		if (checked_text.match(trigger_word)){
-			console.log(`[#] Word: ${trigger_word} found`)
-			return true
-		}
-	}
-	console.log("[#] Clear, moving on...")
-	return false
-}
-
-
-function handleElement(elem){
-	console.log("Handling Element:")
-	console.log(elem)
-	if (containsTrigger(elem)){
-		console.log("Removing Element...")
-		removeElement(elem)
-	}
-}
 
 const elementHandlers = {
 	// Results page
@@ -100,7 +6,16 @@ const elementHandlers = {
 	'ytd-playlist-renderer' : handleElement,
 
 	// Front page
-	'ytd-rich-item-renderer' : handleElement
+	'ytd-rich-item-renderer' : handleElement,
+}
+
+function handleElement(elem){
+	console.log("[TriggerRemover] Handling Element:")
+	console.log(elem)
+	if (containsTrigger(elem)){
+		console.log("[TriggerRemover] Removing Element...")
+		removeElement(elem)
+	}
 }
 
 function removeTriggersFromElement(node){
@@ -125,7 +40,7 @@ function watchResults(targetNode){
 		}
 	};
 
-	console.log("Handling already loaded videos/channels:")
+	console.log("[TriggerRemover] Handling already loaded videos/channels/playlists:")
 	removeTriggersFromElement(targetNode)
 
 	// Create an observer instance linked to the callback function
@@ -135,29 +50,12 @@ function watchResults(targetNode){
 	observer.observe(targetNode, config);
 
 	console.log("================")
-	console.log("Watching:")
+	console.log("[TriggerRemover] Watching results:")
 	console.log(targetNode)
 }
 
 window.onload = function() {
 
-	console.log("Loaded!")
-
-	// Look at the whole document and find the Videos container element
-	const contentsObserver = new MutationObserver((mutations, obs) => {
-
-		const targetNode = document.querySelector('ytd-app');
-
-		if (targetNode) {
-			// When the element is found stop looking!
-			obs.disconnect();
-			watchResults(targetNode)
-			return;
-		}
-	});
-
-	contentsObserver.observe(document, {
-	  childList: true,
-	  subtree: true
-	});
+	console.log("[TriggerRemover] Loaded Youtube Results!")
+	findRootElement('ytd-app', watchResults)
 }
---
+++ assets/js/utils.js
@@ -0,0 +1,132 @@
+// ==================================================
+// Globals
+// ==================================================
+
+TRIGGERS = {}
+
+// ==================================================
+// Functions
+// ==================================================
+
+function loadTriggers(){
+    browser.storage.sync.get(['triggers'])
+        .then((result) => {
+            TRIGGERS = result.triggers;
+            console.log("Trigger words found: " + Object.keys(TRIGGERS).length)
+    });
+}
+
+
+// Taken from:
+// https://stackoverflow.com/a/9310752
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+
+/*
+    Maybe add feature to replace element with custom HTML
+    in the future
+*/
+function removeElement(elem) {
+    elem.remove()
+    // elem.innerText = "This content has been hidden by plugin"
+}
+
+function createTriggerRegex(trigger_word, trigger_entry){
+    regex_flags = "m"   // multiline is the default
+
+    console.log(`[+] Checking for '${trigger_word}'...`)
+    // console.log(trigger_entry)
+
+    // console.log("[1] Checking case_sensitive")
+    // If case_sensitive is NOT set - to lower case
+    if (!trigger_entry["case_sensitive"]) {
+        regex_flags += "i"
+    }
+
+    // console.log("[2] Checking whole_word")
+    // If whole_word is set - create a regex with greedy whitespaces
+    if (trigger_entry["whole_word"]) {
+        trigger_word = `\\s+${trigger_word}\\s+`
+        // console.log(`[2][1] Whole word ${trigger_word}`)
+    }
+
+    // If regex is not set - escape all regex characters
+    // console.log("[3] Checking regex")
+    if (!trigger_entry["regex"]) {
+        // console.log("[3][1] Escape regex chars")
+        trigger_word = escapeRegExp(trigger_word)
+    }
+
+    trigger_word_regex = new RegExp(trigger_word, regex_flags)
+    console.log(`[+] Creating regex for '${trigger_word_regex}'`)
+    return trigger_word_regex
+}
+
+
+function findRootElement(tag, callback){
+    // Look at the whole document and find the Videos container element
+    const contentsObserver = new MutationObserver((mutations, obs) => {
+        const targetNode = document.querySelector(tag);
+
+        if (targetNode) {
+            // When the element is found stop looking!
+            obs.disconnect();
+            callback(targetNode)
+        }
+    });
+
+    // Look at the whole document at first!
+    contentsObserver.observe(document, {
+      childList: true,
+      subtree: true
+    });    
+}
+
+
+function containsTrigger(element){
+    checked_text = element.innerText
+    console.log(checked_text)
+    for (let trigger_word in TRIGGERS){
+        trigger_entry = TRIGGERS[trigger_word]
+        trigger_word_regex = createTriggerRegex(trigger_word, trigger_entry)
+
+        if (checked_text.match(trigger_word_regex)){
+            console.log(`[#] Word: ${trigger_word} found`)
+            return true
+        }
+    }
+    console.log("[#] Clear, moving on...")
+    return false
+}
+
+
+// ==================================================
+// Initial Calls
+// ==================================================
+
+loadTriggers() // Loads the TRIGGERS global
+
+
+
+// ==================================================
+// Unused
+// ==================================================
+
+// Taken from:
+// https://stackoverflow.com/a/39905590
+/*
+String.prototype.fuzzy = function(term, ratio=0.7) {
+    var string = this.toLowerCase();
+    var compare = term.toLowerCase();
+    var matches = 0;
+    if (string.indexOf(compare) > -1) return true; // covers basic partial matches
+    for (var i = 0; i < compare.length; i++) {
+        string.indexOf(compare[i]) > -1 ? matches += 1 : matches -=1;
+    }
+    return (matches/this.length >= ratio || term == "")
+};
+*/
+
+console.log("[TriggerRemover] Utils script loaded")
