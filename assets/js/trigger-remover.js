eb8da8660491cc5afa91f5904361c76bdc0bf675
John Torakis <john.torakis@gmail.com>
2022-09-17 18:38:30 +0300

    bump: try listed

--- assets/js/trigger-remover.js
+++ assets/js/trigger-remover.js
@@ -1,5 +1,5 @@
 const WebExtName = "TriggerRemover"
-const WebExtVersion = "0.1.0"
+const WebExtVersion = "0.1.1"
 const WebExtAuthor = "https://github.com/operatorequals"
 
 const WebExtUrl = browser.runtime.getURL("/")
